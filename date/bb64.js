a459a0d4fa4c524eff965c2dd50f4ce5bfae49bb
igacc <igacc@users.noreply.github.com>
2017-08-31 11:49:49 +0800

    Add files via upload

---
+++ date/bb64.js
@@ -0,0 +1,170 @@
+var bbase={
+    "20170828":"2oiQlNi7lOVilp+zAA4YlXiz2XmM2D4IPa11AA4Y2ci42AwCYya1DwOrmBKS2Oao2jMI2AD32XDtAA4kCtL4K+8yCtL4K+8y2OlzlXazlMy7lKiD2ciu22QI2XmP22QUAA4Y21lS2N4TlpVd2TDd2lQX11++lXyclNiT2p3PAA4Y2T+V2X3KlXyclNiT2X3G2cle2Aab2c4522+m2lVT2pQM2Oao2jMI2jVJ2OlY2AlVAA4YlXyN2omn2clK2O6d2l4Z22D0AA4Y2ctk2Oab21M22XDX2oay2Oab2Tmq2oyq2XmU2oiQ2pmA22D021a321M2AA4Y2XmP2Tab2omn2pyYlct822D02OlzlXazAA4Y2TmC2p3b2p462pmS2XDt2olk21M211++CtL4K+8yCtL4K+8y2c+C21we2N4T2NVD2NtmAA4Y2OlzlXaz2T3R2c4UlNiTAA4Y2plG2oQ/2X362cib2OlLlNQN2N4S2N4T22QIlMiLAA4Y2X3G2omn2pyn21mu22D0AA4Y2XmUlpOa2oQ/2lmP2clwlMtT2lal2XmX2jyU2pDTAA4Y2py82XmDlNlX2OQq2Nwi2plA2Awb2Awf22QI11++CtL4K+8yCtL4K+8y2N4T2AD32X4O2X3X2p3PAA4Y2XmUlKmE2omn2OlzlXaz2TVxlXtI2XmU2Oab2O+tAA4Y2Nwi2Ntx2XmU2Oab2O+tAA4Y2TVx2clelKiD2ciu2j3s2j4L2j4LAA4Y22arlL4rlL4r2N4S2pit2Oag2AD32X4O11++2cQR21aQAA4Y2X3G2XmP2omn21322pmj2OlY2AlV22D011++2oyq2XmD2pQKlMiLlpOa2oQ/2p6F2jwI2XDt2T+T2j+WAA4Y2Oy22TmZ2XmUlOts2o6SlL+dAA4Y2jMI2o4L2p3P2OaJAA4Y2jMI2o4L22arlL4rAA4Y2O6d2O6d2O6dAA4Y2TmZ2pyP224k2pyP224k11++2Nw52DQ9AA4Y2TmZ2XwE2AD32OlO2XDt2p6KlMOk2ptO2clK2OaOlXl22A4p2T+blMtY2Xml2T3PlMtY11++CtL4K+8yCtL4K+8y2jae2jlIAA4Y2N+B2OlY2OlzlXaz22D02AlV2NtxlKM5lNQmAA4Y2OlzlXaz2X3G2plA2jae2jlIAA4Y2jae2OlY2X3X2N+V2l3s2XmM2ADn2jak22D0223dlK34JVOkoVVU5wOK9iaAjVOzoi6KEV6Poy6woVizTimbmiy8HVmnfV6/niOpjyizTi6PmVmbmiiOIVVU5wirmyalIwa833FQAA4Y2pyK2XD42XD4AA4Y2ot12ot111++2OlzlXaz2AwN2TmZ2pyHAA4Y224G22D02OQ32X4O2pwy2XDl2XDtAA4Y2OQ32X4O2TyI22QUlcmI2ci72N4TlKa12XmM2ott2pil2jijlKQI22D02D+v2OQqAA4Y2XmX2j6k2N4S2XmUlXl22N+B2XmUlXl2224X22av2XDl11++2TmZ2X3G2NOr2D4mlXyN2D+vlMyu2TVZAA4Y2TDO2ot122QU2O6222D02ctklKMIAA4YlK6S2ADsAA4Y2NMD2NMDAA4Y2XmUlNmh2XDj2pye11++lKma2o4P22D02XmPlMtYAA4Y2ptQ2c+W2TDR22QU2O6222D0lKaZlMMX2Oy222QU2T+a2oVP2pi52XmXAA4Y21lo2cle2jiu2XmU21M2AA4Y2OlzlXazlKMn2OlO2X4O2o4L2XmPlp++AA4Y2TmZlXyplMOklNiw2O622O6d2XmP2O6d2N+BAA4Y2pDT2TVF22D02O6x2XDl2XwU2X3KlpVd2X4OlOtl2XwV22D0AA4Y2pyD22y/lptRlMlT2O6d22av2XDl22D011++2o4U2N+V2T3H2c+C2l372XDtAA4Y2j3s2j4L2j4L22D0lpiYlNQmAA4Y2TmZ2XmUlXl22TyL2TQC22QU2TyI22QU2O622XmMlNQm22D02O6Q2OQqAA4Y224X2O62lMtHlMtj2XmP2o67AA4YlL6y2Xmz2pOFlMV9lXyp2T4+2N4T2XmU2oQI2OablK6s2XmM11++2TmZ2o4U2NOr2D4m2XwX2XDVlKau2p6L22D02pDT213o2XmV2o672jak22D02XDl2AtA2X3XlNi722D021DQ2T+kAA4Y2DQ92oiQ22D02O622Oau2plA22+t2o3l11++2XmUlXl22c+d2TVF2T3H22QUAA4Y2ot12pil2clK2OaO2j+7AA4Y2T3X2T4g2N4TlpiYlNQm2o+F2pDTAA4YlXyN2XmD2N4S2oO22AlV2OON2pyK2O6d21lo11++2oaylXyc2Xm92XmD2T3X2T4gAA4Y2Tyq2oio2TDM2c+C2l372oMZ2OabAA4Y2TmZ2TVxlXyN2ow72p6F2Oli2pml2jyU2N+b11++22w72pmS2TDd2o+q2X3X2XmfAA4Y2OlzlXazlMlo2TmZ2ptr2XmX11++CtL4K+8yCtL4K+8ylXyN2X3X2N+VAA4Y2TmZ2XwE2pyK2XD42XD4AA4YlX3flX3fAA4YlXyc2plA2XDt2pi52XmXAA4Y2oiQlNi7lXyp2oio2omn2XmPlKVdlXyc2plA22D0AA4Y2XQt2omn2pyK2clK2omn2jm72o4c22D011++2c+d2llI2OlzlXazlMlo2TmZ2pyZ2DyalMDRAA4Y2XQtlXyN2X3G2jMI2o4L2Oau2TlbAA4YlXyc2plAlX3f2pi52XmXlK+Y2ci4AA4Y2c3Q2jMI2o4L2cQm2Tmq2XmU2XmD2T4k2Awf22D0lXyg2lmX11++2XQt2o4U2N+VlXyp2omn2cyP2XmP2XQC2ptO2XDt2l4E2XmU2D4IAA4Y2TmZ2XwElpVdlNif2XmM224822QI2XDt11++2Nw52XmllpVd2clK2NwSAA4Y2XmUlMiL2Xws2XQc22QI2XDt2clK2X3FAA4Y2XQt2pyDlXyg2plA2pQk2pi52pmtlNMG11++lXyp2o4L2Oab2TDM2oiQlNi7AA4Y2pyn2Xw/2T6m2T6m2ptO11++CtL4K+8yCtL4K+8ylclG2Oa72N4TlNm7lptR2pyH2jwZ2pDT22D02TaGlMiLAA4Y2j3s2jO32pyK2AwhlXQnAA4Y2pyK2clK2XD42OytAA4YlXyN2omn2XmU2NQe2Olz2N4S11++21aQ2N+V2pOP2Oy222QU2TVxlXyc2plA22D02oiQ2p+NAA4YlNm7lptR2pyH2omn2Aab2Aab2llP22QU2TmZ22D0AA4Y2O6d2pVC2XmP2p6klMl12TmZlXyc2p6/11++2XQt2OlzlXaz2pyK2omn2jm72o4clMMjlXyc2p6/22D0AA4Y2TmZ2NOr2D4m2XmUlXl22TDu22QUAA4Y2XD422QUAA4YlMy721M222arlL4rlMyzAA4Y2XmXlNQm2XmUlXl2lX3f22QUAA4YlX3f2j3s2XDt2c+u2pyn2Xw/lXyc2p6/lpMH2XmD2l3llNi711++CtL4K+8yCtL4K+8y2l4E2XmL2Oao2XmX2pQKAA4Y2TmZ2XwE2N4T2cDM2XmMAA4Y2XD92Tmp2XmU2X4OAA4Y2TmZ22w12XyIlKiD2ciu22O4lXQnAA4Y2OlzlXaz2pynlKVd2pyH2cle2o4L21M22XmP22ap2cyB11++lpMH2X3KlXyp2omn2TQm2TmZ22D011++lXyg2lmX2oi5lN4U2OaOlMy7AA4Y2AwJ22QU2Nwc2NtTAA4YlK6s2A4zAA4YlpVd2pyn2Xw/lKmWAA4Y2T+A2X3X2o4U2N+V2omn2XmUlNmh2oaA2X4qAA4Y22ww2D+g2c+u2omn2o672XmD22aG2Dl62N3TAA4Y2pyK2N+w2pyK2N+12pyKlKmW2pyK2o+FAA4Y2TVF2TaG2omn2j6k2pDT22D0AA4Y21mu22D021DQ2T+k2X3G2omn2XmP2ptr2pyp2TQm22D0AA4Y2XQt2T+A2o4L2XmU2pmA2O622c4U2OiX2TDd2o+qAA4YlMlo2TmZ2ptr2XmXAA4Y2XQt2TmZlXyp2omn2NOr2D4m2Awb2Awf2TDp2lVT22D0AA4Y2pOP21lo2XDt2O6d2XmU2X4O2T3P2ptr2XmX11++2OlzlXaz2cle2pmS2XDt2oay2oQyAA4YlK+Y2TmZ2Ntt2N4T2plG2N4SAA4Y2D4HlX6C2O6222D02XmX2XQR2TDd2pDTAA4Y2c+ZlMy72X3G2o4L2XmU2Xm92pmtlNMG11++CtL4K+8yCtL4K+8y2o4L2oiQ2p+N2N6D2cle2clK2j3sAA4Y2TmZ2X3G2TaGlMiL2pmSlpiYlNQm2j+72jtD2jtD2XmU213gAA4Y2Oa6lNQm2NtmAA4Y2o672XmD2c+C2l372NtT2Nw7lpVd2omn2j3s22D0AA4Y2OlzlXazlXyp2NtMlMyL2TmZlpVd2j6k2pmS2ctklKMI2XDtAA4Y22D02ltw2j6k2XDt2XmP2c+Z2j+72Ntm11++2XmX2XQR22D02XD92N+KAA4Y2X3G2omn2AlV22D0AA4Y2TmZ2XmU2clU2XmX224XAA4Y2c+u2T+J2AlV22D02pye2XmP2XDtAA4Y2TDd2pil2omn2j6Z2N4t22D0lKML2DVPAA4Y2N+i2p6/2oiQ2p+N2XD92N+K2N4TlpiYlNQm11++2TmZlXyplMlo2OlzlXaz22aT2T3X2ot1lpMHlpiYAA4YlXyc2pil22D0lMi32TaG2X3G2omnlpMH2X3K2AlV11++2N6D2clelXaTlpiCAA4Y2TmZ2jwI2TaC22D0lXyp2omn2p3P2pi52D4IAA4Y2ltE2cD32DyWlXlB2N4T2ADsAA4YlpMH2D+v2pO42oMi2c4U2oQ/2c+u2clK2O6dAA4Y2ptO22QU2ptO22QU2OlzlXaz22D02l372pyH2X4O2Aab2c45lX6v2oQ/AA4YlXyN2liP2Tya2p6/22D022yc22wy2NMY2c452pDc2c+u2ow72o4L2clk2o4P2TaG11++CtL4K+8yCtL4K+8y2c+C21we2pmv2OQp2N4T2TaG2XmP2omnlpMH2X3K2T+a2pmv2XDtAA4Y2DyJ2lwGlX6S2XmM2c3xlNQN22D02Xmf2c37AA4Y21mu21mu2oiQ2p+N2O622T3R2T3S2cle2XmP2OaOAA4Y2c+u21mE2XmMlXyg2XmU2D4I2cDMAA4Y2N4T2oiklXl2lMi+2oto11++2c+u2omn2jMN2pyZ2XmM2TmZ2XwE2XD42Oyt224X22ahlMit2oiQ2p+NAA4Y2O622X4O2NV82NV822QU2jt+21mu2TDOAA4Y2X3G2omnlNQi2cm12TV22XDl2T+r22D011++CtL4K+8yCtL4K+8y2l4E2XDY2Oao2o4L2D4IAA4Y2TmZ2N4T2pQM22QI21DQ2T+kAA4Y2OlzlXaz2pyK2jm72o4clMMj2N+B2O6Q2XDtAA4YlMlo2TmZ2T+V2X3K2T+V2X3K2N+BAA4Y2pyn2omn2TmZ2N+w22QU2c+u2jMI2OaO22D02pDTlNQN2XDt11++lpMH2cyFlOtA22aG2j+W22D0AA4Y2OlzlXaz2pyK2T3flXyg2plAAA4Y2XmPlMlo2TmZ2ot111++2c+C2c+C22D02pmj2T3flpMH2omn2XmP2T6Y22D0AA4Y2TmZ2c+u2XQs2pD42XD42XDtAA4Y2XwV2Oa72XD42pmS2c+yAA4Y224X2XQ52Twa2Awz2N+311++2XD422QU2XD422QUlKau2p6L2p6LAA4Y224G2omn2NVD2paslpVd2XD4AA4Y2cyp2XDt2X3X2p3P22D0lpVklNivAA4Y2OlL21aQ2Tl/2pye2TmZ22D02XD42N+A11++CtL4K+8yCtL4K+8y2O6d2XDtAA4YlN392XDt2N+02liP2XD42OytlMtY2XmlAA4Y2TmZ2XwE2X3G2ptO2XDt2pmj22D02XDX2TVFAA4Y2cQR21aQ2Nyl2o4E2omn2OlzlXaz2ptO2XDX11++2TmZlXaGlXaH2NVk2c+zAA4Y2AV6lpOX2TlalODTAA4Y21MalOONlKygAA4Y2pOE22Vr2jt9AA4Y2j+7216w22D0lNQalKyrAA4YlpVd2omn2olk2O6d22D011++2TmZ2X3G2NOr2D4m2A4522QU2OlzlXaz2NMY2O62224X2T3X2o4lAA4Y2DyJ2lwG2pmv2cm62X3G2omn2OlzlXaz2XmU2Oab21mu2O6dAA4YlL3dlMy72clK2OaOlN6s2lyg22D02p6j2pQ32X3G2XmP22y/2O622T+V2X3K224X2cle2XmX2plA11++2TQC2TQC2T3XAA4Y2T4L2oto2T4L2otoAA4Y2c+u2XmP2p+82clelMy72XDtAA4Y2TmZ2X3G2jMI2o4L22aTlO3ulOVV2jM22jO+2XmUlptP2oQ/2XmD2cQA2cDJ22D02T4L2otoAA4Y21lS2N4T2o4L21M22N+V2TMW11++2cQA2cDJ22D02T4L2oto2c+u2omn2clelMlo2OlzlXazlKma2o4P2clelpVd2XmP2TVx21lo2T3X2o4l22D0AA4Y2ctF21aQlpte2j4C2XDt11++lKix2XDV2TVF2XDl2X3XlNi722D02Oy2lMyzAA4Y2X3G2jMI2N6/21322pmjAA4Y2X3G2Xw92XDllXQA2oQyAA4Y2NMY2OlzlXazlMy72cDGlMyzAA4Y2XmP2ptr2N4S2Oa12OlzlXazAA4Y224X2O622OO+2XQJ2XQRlNQm2N4S2cDW2Oy2AA4Y2X3G2omn2clK2O6d21lo11++",
+}
+/**
+* UTF16和UTF8转换对照表
+* U+00000000 – U+0000007F 	0xxxxxxx
+* U+00000080 – U+000007FF 	110xxxxx 10xxxxxx
+* U+00000800 – U+0000FFFF 	1110xxxx 10xxxxxx 10xxxxxx
+* U+00010000 – U+001FFFFF 	11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+* U+00200000 – U+03FFFFFF 	111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+* U+04000000 – U+7FFFFFFF 	1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+*/
+var Base64 = {
+	// 转码表
+	//table : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split(""),
+	//table : "Uk+B0FtgKLMXYPVCqZaRWJ6epNOcrziG5ImH9/3bToDjEfwnSu4x7hQv12lA8dys".split(""),
+	UTF16ToUTF8 : function(str) {
+		var res = [], len = str.length;
+		for (var i = 0; i < len; i++) {
+			var code = str.charCodeAt(i);
+			if (code > 0x0000 && code <= 0x007F) {
+				// 单字节，这里并不考虑0x0000，因为它是空字节
+				// U+00000000 – U+0000007F 	0xxxxxxx
+				res.push(str.charAt(i));
+			} else if (code >= 0x0080 && code <= 0x07FF) {
+				// 双字节
+				// U+00000080 – U+000007FF 	110xxxxx 10xxxxxx
+				// 110xxxxx
+				var byte1 = 0xC0 | ((code >> 6) & 0x1F);
+				// 10xxxxxx
+				var byte2 = 0x80 | (code & 0x3F);
+				res.push(
+					String.fromCharCode(byte1), 
+					String.fromCharCode(byte2)
+				);
+			} else if (code >= 0x0800 && code <= 0xFFFF) {
+				// 三字节
+				// U+00000800 – U+0000FFFF 	1110xxxx 10xxxxxx 10xxxxxx
+				// 1110xxxx
+				var byte1 = 0xE0 | ((code >> 12) & 0x0F);
+				// 10xxxxxx
+				var byte2 = 0x80 | ((code >> 6) & 0x3F);
+				// 10xxxxxx
+				var byte3 = 0x80 | (code & 0x3F);
+				res.push(
+					String.fromCharCode(byte1), 
+					String.fromCharCode(byte2), 
+					String.fromCharCode(byte3)
+				);
+			} else if (code >= 0x00010000 && code <= 0x001FFFFF) {
+				// 四字节
+				// U+00010000 – U+001FFFFF 	11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+			} else if (code >= 0x00200000 && code <= 0x03FFFFFF) {
+				// 五字节
+				// U+00200000 – U+03FFFFFF 	111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+			} else /** if (code >= 0x04000000 && code <= 0x7FFFFFFF)*/ {
+				// 六字节
+				// U+04000000 – U+7FFFFFFF 	1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+			}
+		}
+
+		return res.join('');
+	},
+	UTF8ToUTF16 : function(str) {
+		var res = [], len = str.length;
+		var i = 0;
+		for (var i = 0; i < len; i++) {
+			var code = str.charCodeAt(i);
+			// 对第一个字节进行判断
+			if (((code >> 7) & 0xFF) == 0x0) {
+				// 单字节
+				// 0xxxxxxx
+				res.push(str.charAt(i));
+			} else if (((code >> 5) & 0xFF) == 0x6) {
+				// 双字节
+				// 110xxxxx 10xxxxxx
+				var code2 = str.charCodeAt(++i);
+				var byte1 = (code & 0x1F) << 6;
+				var byte2 = code2 & 0x3F;
+				var utf16 = byte1 | byte2;
+				res.push(Sting.fromCharCode(utf16));
+			} else if (((code >> 4) & 0xFF) == 0xE) {
+				// 三字节
+				// 1110xxxx 10xxxxxx 10xxxxxx
+				var code2 = str.charCodeAt(++i);
+				var code3 = str.charCodeAt(++i);
+				var byte1 = (code << 4) | ((code2 >> 2) & 0x0F);
+				var byte2 = ((code2 & 0x03) << 6) | (code3 & 0x3F);
+				utf16 = ((byte1 & 0x00FF) << 8) | byte2
+				res.push(String.fromCharCode(utf16));
+			} else if (((code >> 3) & 0xFF) == 0x1E) {
+				// 四字节
+				// 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+			} else if (((code >> 2) & 0xFF) == 0x3E) {
+				// 五字节
+				// 111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+			} else /** if (((code >> 1) & 0xFF) == 0x7E)*/ {
+				// 六字节
+				// 1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+			}
+		}
+
+		return res.join('');
+	},
+	encode : function(str,table) {
+		if (!str) {
+			return '';
+		}
+		var utf8    = this.UTF16ToUTF8(str); // 转成UTF8
+		var i = 0; // 遍历索引
+		var len = utf8.length;
+		var res = [];
+		while (i < len) {
+			var c1 = utf8.charCodeAt(i++) & 0xFF;
+			res.push(table[c1 >> 2]);
+			// 需要补2个=
+			if (i == len) {
+				res.push(table[(c1 & 0x3) << 4]);
+				res.push('==');
+				break;
+			}
+			var c2 = utf8.charCodeAt(i++);
+			// 需要补1个=
+			if (i == len) {
+				res.push(table[((c1 & 0x3) << 4) | ((c2 >> 4) & 0x0F)]);
+				res.push(table[(c2 & 0x0F) << 2]);
+				res.push('=');
+				break;
+			}
+			var c3 = utf8.charCodeAt(i++);
+			res.push(table[((c1 & 0x3) << 4) | ((c2 >> 4) & 0x0F)]);
+			res.push(table[((c2 & 0x0F) << 2) | ((c3 & 0xC0) >> 6)]);
+			res.push(table[c3 & 0x3F]);
+		}
+
+		return res.join('');
+	},
+	decode : function(str,table) {
+		if (!str) {
+			return '';
+		}
+
+		var len = str.length;
+		var i   = 0;
+		var res = [];
+
+		while (i < len) {
+			code1 = table.indexOf(str.charAt(i++));
+			code2 = table.indexOf(str.charAt(i++));
+			code3 = table.indexOf(str.charAt(i++));
+			code4 = table.indexOf(str.charAt(i++));
+
+			c1 = (code1 << 2) | (code2 >> 4);
+			c2 = ((code2 & 0xF) << 4) | (code3 >> 2);
+			c3 = ((code3 & 0x3) << 6) | code4;
+
+			res.push(String.fromCharCode(c1));
+
+			if (code3 != 64) {
+				res.push(String.fromCharCode(c2));
+			}
+			if (code4 != 64) {
+				res.push(String.fromCharCode(c3));
+			}
+
+		}
+
+		return this.UTF8ToUTF16(res.join(''));
+	}
+};
